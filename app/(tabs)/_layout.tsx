c79b863621f61b063792cfbe909bcefdfa505db9
Frankie <frankiejosefej@gmail.com>
2024-06-06 14:48:28 -0400

    Finished setting up tabs and styling them

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Layout = () => {
   return (
@@ -19,6 +18,38 @@ const Layout = () => {
                 tabBarLabel: 'Explore',
                 tabBarIcon: ({ color, size }) => 
                 <Ionicons name='search' color={color} size={size} />
+            }} 
+        />
+        <Tabs.Screen 
+            name='wishlists' 
+            options={{
+                tabBarLabel: 'Wishlists',
+                tabBarIcon: ({ color, size }) => 
+                <Ionicons name='heart-outline' color={color} size={size} />
+            }} 
+        />
+        <Tabs.Screen 
+            name='trips' 
+            options={{
+                tabBarLabel: 'Trips',
+                tabBarIcon: ({ color, size }) => 
+                <FontAwesome5 name='airbnb' color={color} size={size} />
+            }} 
+        />
+        <Tabs.Screen 
+            name='inbox' 
+            options={{
+                tabBarLabel: 'Inbox',
+                tabBarIcon: ({ color, size }) => 
+                <MaterialCommunityIcons name='message-outline' color={color} size={size} />
+            }} 
+        />
+        <Tabs.Screen 
+            name='profile' 
+            options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color, size }) => 
+                <Ionicons name='person-circle-outline' color={color} size={size} />
             }} 
         />
     </Tabs>
