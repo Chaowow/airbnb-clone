dc2e52aaf32b17f04410705a4514c323f6b0da7f
Frankie <frankiejosefej@gmail.com>
2024-07-17 20:56:09 -0400

    Styled search button and filter button

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const booking = () => {
   return (
     <View>
-      <Text>Booking</Text>
+      <Text>BOOK</Text>
     </View>
   );
 };
--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -42,7 +42,17 @@ const ExploreHeader = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
         <View style={styles.container}>
             <View style={styles.actionRow}>
-                <Link href={'/(modals)/booking'}>Booking</Link>
+                <Link href={'/(modals)/booking'} asChild>
+                    <TouchableOpacity style={styles.searchBtn}>
+                        <Ionicons name='search' size={24}/>
+                        <View>
+                            <Text style={{ fontFamily: 'mon-sb' }}>Where to?</Text>
+                            <Text style={{ fontFamily: 'mon', color: Colors.grey }}>
+                                Anywhere ∙ Any week
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </Link>
 
                 <TouchableOpacity style={styles.filterBtn}>
                     <Ionicons name='options-outline' size={24} />
@@ -63,13 +73,33 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingHorizontal: 24,
-        paddingBottom: 16
+        paddingBottom: 16,
+        gap: 10
     },
     filterBtn: {
         padding: 10,
         borderWidth: 1,
         borderColor: Colors.grey,
         borderRadius: 24
+    },
+    searchBtn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        borderColor: '#c2c2c2',
+        borderWidth: StyleSheet.hairlineWidth,
+        flex: 1,
+        padding: 14,
+        borderRadius: 30,
+        backgroundColor: '#fff',
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.12,
+        shadowRadius: 8,
+        shadowOffset: {
+            width: 1,
+            height: 1
+        }
     }
 });
 
