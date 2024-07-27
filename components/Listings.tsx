ed6b6b50eb5f064cf6fcafdb86fbf8d8e933b9a1
Frankie <frankiejosefej@gmail.com>
2024-07-27 02:39:17 -0400

    Implemented images, review scores, and name of places and added typescript interface

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,7 +1,17 @@
-import { View, Text, FlatList, ListRenderItem } from 'react-native';
+import { 
+  View, 
+  Text, 
+  FlatList, 
+  ListRenderItem, 
+  TouchableOpacity, 
+  StyleSheet,
+  Image
+} from 'react-native';
 import React, { useEffect, useRef, useState } from 'react';
 import { defaultStyles } from '@/constants/Styles';
 import { Link } from 'expo-router';
+import { Listing } from '@/interfaces/listing';
+import { Ionicons } from '@expo/vector-icons';
 
 interface Props {
     listings: any[];
@@ -21,9 +31,34 @@ const Listings = ({ listings: items, category }: Props) => {
       }, 200);
   }, [category]);
 
-  const renderRow: ListRenderItem<any> = ({ item }) => (
-    <Link href={`/listing/${item.id}`}>
-      Go there
+  const renderRow: ListRenderItem<Listing> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+      <View style={styles.listing}>
+          {item.medium_url ? (
+            <Image source={{ uri: item.medium_url }} style={styles.image} />
+          ) : (
+            <View style={styles.imagePlaceholder} />
+          )}
+          <TouchableOpacity style={{ position: 'absolute', right: 30, top: 30}}>
+            <Ionicons name='heart-outline' size={24} color={'#000'}/>
+          </TouchableOpacity>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 15, fontFamily: 'mon-sb'}}>{item.name}</Text>
+            <View style={{ flexDirection: 'row', gap: 4 }}>
+              <Ionicons name='star' size={16} />
+              {item.review_scores_rating != null ? (
+                <Text style={{ fontFamily: 'mon-sb'}}>
+                  {item.review_scores_rating / 20}
+                </Text>
+              ) : (
+                <Text style={{ fontFamily: 'mon-sb'}}>N/A</Text>
+              )}
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
     </Link>
   )
 
@@ -37,5 +72,23 @@ const Listings = ({ listings: items, category }: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10
+  },
+  imagePlaceholder: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ccc',
+  },
+})
 
 export default Listings;
