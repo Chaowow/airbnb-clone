ba94815f457e6e4c2ec722d6f7ce44041bf867a8
Frankie <frankiejosefej@gmail.com>
2024-07-31 15:35:36 -0400

    Finsihed implementing basics and simple styling for listings

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -45,17 +45,23 @@ const Listings = ({ listings: items, category }: Props) => {
           </TouchableOpacity>
 
           <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Text style={{ fontSize: 15, fontFamily: 'mon-sb'}}>{item.name}</Text>
+            <Text style={{ fontSize: 15, fontFamily: 'mon-sb' }}>{item.name}</Text>
             <View style={{ flexDirection: 'row', gap: 4 }}>
               <Ionicons name='star' size={16} />
               {item.review_scores_rating != null ? (
-                <Text style={{ fontFamily: 'mon-sb'}}>
+                <Text style={{ fontFamily: 'mon-sb' }}>
                   {item.review_scores_rating / 20}
                 </Text>
               ) : (
-                <Text style={{ fontFamily: 'mon-sb'}}>N/A</Text>
+                <Text style={{ fontFamily: 'mon-sb' }}>N/A</Text>
               )}
             </View>
+          </View>
+
+          <Text style={{ fontFamily: 'mon'}}>{item.room_type}</Text>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+            <Text style={{ fontFamily: 'mon'}}>night</Text>
           </View>
         </View>
       </TouchableOpacity>
