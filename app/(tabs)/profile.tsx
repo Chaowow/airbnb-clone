3b56fbe45d764b0fb19da6de3c0f9887024a2af1
Frankie <frankiejosefej@gmail.com>
2024-08-22 03:41:31 -0400

    Started implementing user profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,97 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
-import { useAuth } from '@clerk/clerk-expo';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useAuth, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEmail(user.emailAddresses[0].emailAddress);
+  }, [user]);
+
+  const onSaveUser = async () => {
+
+  }
+
+  const onCaptureImage = async () => {
+
+  }
 
   return (
-    <View>
-      <Button title='Log Out' onPress={() => signOut()} />
-        {!isSignedIn && (
-          <Link href={'/(modals)/login'}>
-            <Text>Login</Text>
-          </Link>
-        )}
-    </View>
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name='notifications-outline' size={26} />
+      </View>
+
+      {user && <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </TouchableOpacity>
+          
+        </View>}
+
+      {isSignedIn && 
+      <Button title='Log out' onPress={() => signOut()} color={Colors.dark}/>}
+
+      {!isSignedIn && (
+        <Link href={'/(modals)/login'} asChild>
+          <Button title='Log In' color={Colors.dark} />
+        </Link>
+      )}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 24
+  },
+  header: {
+    fontFamily: 'mon-sb',
+    fontSize: 24
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2
+    },
+    alignItems: 'center',
+    gap: 14,
+    marginBottom: 24
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey
+  }
+});
 
 export default Page;
